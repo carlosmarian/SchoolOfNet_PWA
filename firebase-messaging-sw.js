06f5ad6fdc38c80c6cd21ef7ee7f5105d55eb895
Carlos H. Marian <carlosmarian@gmail.com>
2018-04-05 21:13:20 -0300

    Ajustes nos scripts do fcm

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,7 +1,5 @@
-//importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
-//importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
-importScripts('https://www.gstatic.com/firebasejs/4.9.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.9.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
 
 firebase.initializeApp({
     'messagingSenderId': "990094686790"
