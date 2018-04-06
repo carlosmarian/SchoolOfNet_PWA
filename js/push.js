b779af4185d6fb21c964c959369c7b541521772a
Carlos H. Marian <carlosmarian@gmail.com>
2018-04-05 21:07:22 -0300

    Ajustes nos codigos para teste s

--- js/push.js
+++ js/push.js
@@ -1,12 +1,13 @@
 console.log('[Application] start push listening');
 
+/*
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./firebase-messaging-sw.js')
         .then(registration => {
-
+*/
             const messaging = firebase.messaging();
 
-            messaging.useServiceWorker(registration)
+           // messaging.useServiceWorker(registration)
 
             
             messaging.requestPermission().then(function () {
@@ -14,27 +15,32 @@ if ('serviceWorker' in navigator) {
 
                 return messaging.getToken().then(function (currentToken) {
                     if (currentToken) {
-                        console.log(currentToken);
-                        return currentToken;
-                    } else {
-                        console.warn('Nenhum id disponível, Solicite permissão apra gerar um');
-                    }
-                });
-            });
-
-            messaging.getToken()
-                .then(function(currentToken) {
-                    if (currentToken) {
-                        console.log(currentToken);
+                        console.log('TOKEN_1: '+currentToken);
                         return currentToken;
                     } else {
                         console.warn('Nenhum id disponível, Solicite permissão apra gerar um');
                     }
                 })
                 .catch(function(err) {
+                    console.warn('get token err 2', err);
+                });
+            });
+
+            messaging.getToken()
+                .then(function(currentToken) {
+                    if (currentToken) {
+                        console.log('TOKEN_2: '+currentToken);
+                        return currentToken;
+                    } else {
+                        console.warn('Nenhum id disponível, Solicite permissão apra gerar um 2');
+                    }
+                })
+                .catch(function(err) {
                     console.warn('get token err', err);
                 });
-
+/*
         })
         .catch(err => console.log('Service Worker Error', err))
 }
+
+*/
